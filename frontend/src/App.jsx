66aa9625d1c5ce4e3490fdede08198b985492047
lionjumama <driverbaby307@gmail.com>
2024-12-21 03:16:28 +0530

    Fix casing issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
 
 function App() {
   
 
   return (
     <>
-      <div>hello ji </div>
+<Header />
+<Body />
+<Footer />
     </>
   )
 }
