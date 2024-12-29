497ca3c4d439e0bc309281744e064ae59fb9f668
lionjumama <driverbaby307@gmail.com>
2024-12-29 20:00:09 +0530

    aa

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -64,10 +64,10 @@ const About = () => {
     <div className="flex-grow bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white py-24 px-6 relative">
       {/* About Section */}
       <div className="max-w-7xl mx-auto text-center">
-        <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold glow-effect mb-6 transform transition-all duration-500 ease-in-out hover:scale-105">
+        <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold glow-effect mb-6 transform transition-all duration-500 ease-in-out " >
           About Chat-Zone
         </h1>
-        <p className="text-sm md:text-lg lg:text-2xl mb-8 opacity-80 transform transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-100">
+        <p className="text-sm md:text-lg lg:text-2xl mb-8 opacity-80 transform transition-all duration-300 ease-in-ou ">
           Discover a platform where real-time communication meets simplicity and security.
         </p>
 
