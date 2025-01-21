fe7043fcbe77c0a6b73b9f68b9b7e0ba89e942dc
lionjumama <driverbaby307@gmail.com>
2025-01-22 03:15:03 +0530

    feat: implement conditional rendering of Header and Footer in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 import Login from "./pages/Login";
@@ -8,27 +8,44 @@ import About from "./pages/About";
 import Footer from "./Components/Footer";
 import Features from "./pages/Features";
 import ForgetPass from "./Components/ForgetPassword";
-
+import Chat from "./Components/Chat"; // Chat component import
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-gray-900">
-        <Header />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Body />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} /> 
-            <Route path="/features" element={<Features />} /> 
-            <Route path="/forgot-password" element={<ForgetPass />} /> 
-          </Routes>
-        </div>
-        <Footer />
-      </div>
+      <AppLayout />
     </Router>
   );
 };
 
+const AppLayout = () => {
+  const location = useLocation(); // Get current route location
+
+  // Define routes where Header and Footer should not be shown
+  const noHeaderFooterRoutes = ["/chat"];
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-900">
+      {/* Conditionally render Header and Footer */}
+      {!noHeaderFooterRoutes.includes(location.pathname) && <Header />}
+
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/forgot-password" element={<ForgetPass />} />
+          <Route path="/chat" element={<Chat />} /> {/* Chat page route */}
+        </Routes>
+      </div>
+
+      {/* Conditionally render Footer */}
+      {!noHeaderFooterRoutes.includes(location.pathname) && <Footer />}
+    </div>
+  );
+};
+
 export default App;
+
