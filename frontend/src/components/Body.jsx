1e82ca984d87eafd514811722aa1274970f2fda2
Chirag Sharma <168902489+ChiragSharma011@users.noreply.github.com>
2025-01-23 18:13:45 +0530

    feat: update button and link styles to use blue and purple color scheme

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -6,26 +6,28 @@ const Body = () => {
 
   return (
     <div className="flex-grow bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white py-24 px-6 relative overflow-hidden flex items-center justify-center min-h-screen">
-      {/* Main Hero Section */}
-      <div className="max-w-7xl mx-auto text-center">
-        <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold glow-effect mb-6 transform transition-all duration-500 ease-in-out hover:scale-105">
-          Connect with People Like Never Before
-        </h1>
-        <p className="text-sm md:text-lg lg:text-2xl mb-8 opacity-80 transform transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-100">
-          Chat, date, and build relationships seamlessly with our advanced platform.
-        </p>
-        <button
-          onClick={() => navigate('/register')} // Navigate to Register page
-          className="px-8 py-4 md:px-10 md:py-5 bg-gradient-to-r from-blue-400 to-purple-500 text-black font-bold rounded-full shadow-xl transform transition duration-500 hover:bg-blue-300 hover:scale-110"
-        >
-          Start Chatting Now
-        </button>
+      <div className="max-w-7xl mx-auto flex w-full">
+        {/* Left Section */}
+        <div className="w-1/2 text-left">
+          <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold glow-effect mb-6 transform transition-all duration-500 ease-in-out hover:scale-105" style={{ fontFamily: 'MonstrousFish' }}>
+            Connect with People Like Never Before
+          </h1>
+          <p className="text-sm md:text-lg lg:text-2xl mb-8 opacity-80 transform transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-100" style={{ fontFamily: 'RacingSportDisplay-Regular' }}>
+            Chat, date, and build relationships seamlessly with our advanced platform.
+          </p>
+          <button
+            onClick={() => navigate('/register')} // Navigate to Register page
+            className="px-8 py-4 md:px-10 md:py-5 bg-gradient-to-r from-blue-400 to-purple-500 text-black font-bold rounded-full shadow-xl transform transition duration-500 hover:bg-blue-300 hover:scale-110"
+            style={{ fontFamily: 'RacingSportDisplay-Regular' }}
+          >
+            Start Chatting Now
+          </button>
+        </div>
+        {/* Right Section */}
+        <div className="w-1/2"></div>
       </div>
     </div>
   );
 };
 
 export default Body;
-
-
-
