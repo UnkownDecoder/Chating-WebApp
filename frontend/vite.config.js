6ab268019cc97ea5831673e1e468096d4e9230d5
DecoderxVishal <146518151+UnkownDecoder@users.noreply.github.com>
2025-02-01 20:31:22 +0530

    feat: add esbuild as a development dependency and update Vite configuration for dependency optimization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,22 +6,30 @@ export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
-      // Allow access to node_modules, public directory, and other relevant paths
       allow: [
         path.resolve(__dirname, ''),
         path.resolve(__dirname, 'node_modules'),
         path.resolve(__dirname, 'public'),
-        path.resolve(__dirname, 'src/Components/sounds'), // Add sounds directory
+        path.resolve(__dirname, 'src/Components/sounds'),
       ],
     },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Simplify imports with '@'
+      '@': path.resolve(__dirname, 'src'),
     },
   },
+  optimizeDeps: {
+    exclude: [
+      '@mapbox/node-pre-gyp',   // Ensure node-pre-gyp is excluded from frontend bundle
+      'nock',
+      'aws-sdk',
+      'mock-aws-s3',
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@mapbox/node-pre-gyp'], // Mark node-pre-gyp as external if needed
+    }
+  }
 });
-
-
-
-
